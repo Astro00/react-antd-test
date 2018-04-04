18997e8bdf6fb073dcc31b189db04fd9e5654db3
jianglei <jarrelljl@163.com>
2018-04-04 22:05:15 +0800

    cli-pro

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Table, Input, Popconfirm } from 'antd';
+import registerServiceWorker from './registerServiceWorker';
+
+class EditableTable extends React.Component {
+
+}
+
+ReactDOM.render(
+    <EditableTable />,
+    document.getElementById('root')
+);
+registerServiceWorker();
